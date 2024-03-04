940fb15ec2f34bae61a375e548f35e7dd94a6b5e
ssvgklyncu <158519720+ssvgklyncu@users.noreply.github.com>
2024-03-04 23:01:11 +0300

    Number guess app
    Number guess app c#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("sayı tahmin oyunu:");
+            
+            Console.WriteLine("aklınızdan sayı tutunuz:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("tuttuğunuz sayı:");
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
